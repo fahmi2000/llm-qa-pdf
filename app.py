ab5767bb4bfb46671f49475705334b394fa34579
Meyrin <68102284+fahmi2000@users.noreply.github.com>
2023-11-15 09:32:01 +0800

    refactoring hugginface depreciation
    initial commit

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores.faiss import FAISS
 from langchain.chat_models import ChatOpenAI
+from langchain.llms import HuggingFaceHub
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
@@ -37,7 +38,8 @@ def get_vectorstore(text_chunks):
     return vectorstore
     
 def get_converse_chain(vectorstore):
-    llm = ChatOpenAI()
+    # llm = ChatOpenAI()
+    llm = HuggingFaceHub(repo_id = "google/flan-t5-xxl", model_kwargs={"temperature": 0.5, "max_length": 512})
     memory = ConversationBufferMemory(memory_key = 'chat_history', return_messages = True)
     converse_chain = ConversationalRetrievalChain.from_llm(
         llm = llm,
