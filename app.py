57c71dfdde1ce2de78d3d1e49090df27e9182baa
Meyrin <68102284+fahmi2000@users.noreply.github.com>
2023-11-15 10:29:48 +0800

    debugging empty 'sentence' list

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.vectorstores.faiss import FAISS
-from langchain.chat_models import ChatOpenAI
-from langchain.llms import HuggingFaceHub
-from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
+from huggingface_hub import InferenceClient
+from langchain.chains.conversation.memory import ConversationBufferMemory
+
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -37,15 +37,33 @@ def get_vectorstore(text_chunks):
     vectorstore = FAISS.from_texts(texts = text_chunks, embedding = embeddings)
     return vectorstore
     
+# def get_converse_chain(vectorstore):
+#     # llm = ChatOpenAI()
+#     llm = HuggingFaceHub(repo_id = "google/flan-t5-xxl", model_kwargs={"temperature": 0.5, "max_length": 512})
+#     memory = ConversationBufferMemory(memory_key = 'chat_history', return_messages = True)
+#     converse_chain = ConversationalRetrievalChain.from_llm(
+#         llm = llm,
+#         retriever = vectorstore.as_retriever(),
+#         memory = memory
+#     )
+#     return converse_chain
+
 def get_converse_chain(vectorstore):
-    # llm = ChatOpenAI()
-    llm = HuggingFaceHub(repo_id = "google/flan-t5-xxl", model_kwargs={"temperature": 0.5, "max_length": 512})
-    memory = ConversationBufferMemory(memory_key = 'chat_history', return_messages = True)
+    try:
+        llm = InferenceClient(model="google/flan-t5-xxl")
+        print("Model loaded successfully!")
+    except Exception as e:
+        print("Failed to load model!")
+        print("Error: ", e)
+        return None
+
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
     converse_chain = ConversationalRetrievalChain.from_llm(
-        llm = llm,
+        llm=llm,
         retriever = vectorstore.as_retriever(),
         memory = memory
     )
+    
     return converse_chain
 
 def handle_userinput(user_question):
@@ -75,17 +93,18 @@ def main():
         
         if st.button("Process"):
             with st.spinner("Processing"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)                       
+                raw_text = get_pdf_text(pdf_docs)
                 
-                # get text chunks
-                text_chunks = get_text_chunks(raw_text)
+                # text_chunks = get_text_chunks(raw_text)
                 
-                # create vector store
-                vectorstore = get_vectorstore(text_chunks)
-                
-                # create conversation chain
-                st.session_state.converse = get_converse_chain(vectorstore)
+                print("Raw Text:", raw_text)
+
+                # vectorstore = get_vectorstore(text_chunks)
+                # print("Vectorstore:", vectorstore)
+
+                # st.session_state.converse = get_converse_chain(vectorstore)
+                # print("Converse Chain:", st.session_state.converse)
+
 
 if __name__ == '__main__':
     main()
