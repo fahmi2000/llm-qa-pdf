160c01903d7315d2c8fd6743bedde7fbedcaa1df
Meyrin <68102284+fahmi2000@users.noreply.github.com>
2023-11-20 14:17:24 +0800

    updated app.py
    removed OpenAI related imports
    changed llm to oasst sft-4
    added pickle file for file ingest logic

--- app.py
+++ app.py
@@ -1,110 +1,90 @@
 import streamlit as st
 from dotenv import load_dotenv
+import pickle
 from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
+from streamlit_extras.add_vertical_space import add_vertical_space
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
-from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+import os
 
-
-def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
-
-
-def get_text_chunks(text):
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-    )
-    chunks = text_splitter.split_text(text)
-    return chunks
-
-
-def get_vectorstore(text_chunks):
-    # embeddings = OpenAIEmbeddings()
-    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-    return vectorstore
-
-
-def get_conversation_chain(vectorstore):
-    # Instantiate the local LLM
-    tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-large")
-    model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-large")
-
-    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
-
-    def local_llm(input_text):
-        input_ids = tokenizer(input_text, return_tensors="pt").input_ids
-        outputs = model.generate(input_ids)
-        return tokenizer.decode(outputs[0])
-
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=local_llm,
-        retriever=vectorstore.as_retriever(),
-        memory=memory
-    )
-    return conversation_chain
-
-def handle_userinput(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chat_history = response['chat_history']
-
-    for i, message in enumerate(st.session_state.chat_history):
-        if i % 2 == 0:
-            st.write(user_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
-        else:
-            st.write(bot_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
-
+with st.sidebar:
+    st.title('LLM Chat PoC')
+    st.markdown('''
+    ## About
+    This app is an LLM-powered chatbot using:
+    - Streamlit
+    - LangChain
+    - OpenAI
+    
+    ''')
+    
+    add_vertical_space(5)
+    st.write('Made agonizingly by me....')    
+    
 
 def main():
+    st.write("Hello")
+    
     load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs",
-                       page_icon=":books:")
-    st.write(css, unsafe_allow_html=True)
+    
+    pdf = st.file_uploader("Upload your PDF", type='pdf')
+    
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        
+        text = ""
+        
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+            
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 1000,
+            chunk_overlap = 200,
+            length_function = len
+        )
+        
+        chunks = text_splitter.split_text(text = text)
+        
+        db_folder = 'db'
+        store_name = pdf.name[:-4]
+        pkl_path = os.path.join(db_folder, f"{store_name}.pkl")
+        
+        if os.path.exists(pkl_path):
+            with open(pkl_path, "rb") as f:
+                VectorStore = pickle.load(f)
+            st.write('Embeddings loaded locally.')
+        else:
+            embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
 
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = None
+            # Construct the path to the .pkl file in the 'db' folder
+            pkl_path = os.path.join(db_folder, f"{store_name}.pkl")
 
-    st.header("Chat with multiple PDFs :books:")
-    user_question = st.text_input("Ask a question about your documents:")
-    if user_question:
-        handle_userinput(user_question)
-
-    with st.sidebar:
-        st.subheader("Your documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-        if st.button("Process"):
-            with st.spinner("Processing"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
-
-                # get the text chunks
-                text_chunks = get_text_chunks(raw_text)
-
-                # create vector store
-                vectorstore = get_vectorstore(text_chunks)
-
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
-
+            with open(pkl_path, "wb") as f:
+                pickle.dump(VectorStore, f)
+            st.write("New embeddings computed.")
+        
+        query = st.text_input("Ask questions about your document:")
+        # st.write(query)
+        
+        if query:
+            docs = VectorStore.similarity_search(query = query, k = 3)
+            
+            # llm = OpenAI(model_name = "gpt-3.5-turbo")
+            llm = HuggingFaceHub(repo_id="OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5", model_kwargs={"temperature":0.8, "max_length":512})
+            
+            chain = load_qa_chain(llm = llm, chain_type = "stuff")
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents = docs, question = query)
+                print(cb)
+            
+            st.write(response)
+            print(response)     
+            st.write(docs)
 
 if __name__ == '__main__':
     main()
