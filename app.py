c9b6b922f5b4427e019c5dfba4dc81e5ba134c24
Meyrin <68102284+fahmi2000@users.noreply.github.com>
2023-11-21 11:02:27 +0800

    add message history

--- app.py
+++ app.py
@@ -84,6 +84,23 @@ def main():
             ('oasst-sft-4', 'flan-t5-xxl', 'falcon-7b')
         )
 
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # Accept user input
+    if prompt := st.chat_input("What is up?"):
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+    
+    
     query = st.chat_input("Ask questions about your document...")
 
     if query:
