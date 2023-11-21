15f4c300c9ce64f7b52a548884a09ea44d85ec4a
Meyrin <68102284+fahmi2000@users.noreply.github.com>
2023-11-21 09:34:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,59 +11,48 @@ from langchain.chains.question_answering import load_qa_chain
 import os
 
 with st.sidebar:
-    st.title('LLM Chat PoC')
-    st.markdown('''
-    ## About
-    This app is an LLM-powered chatbot using:
-    - Streamlit
-    - LangChain
-    - OpenAI
-    
-    ''')
-    
-    add_vertical_space(5)
-    st.write('Made agonizingly by me....')    
+    st.title('LLM Chat')
                     
 def main():
     load_dotenv()
     
     with st.sidebar:
         pdf = st.file_uploader("Upload your PDF", type='pdf')
-    
-    if pdf is not None:
-        pdf_reader = PdfReader(pdf)
-        
-        text = ""
-        
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-            
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size = 1000,
-            chunk_overlap = 200,
-            length_function = len
-        )
-        
-        chunks = text_splitter.split_text(text = text)
-        
-        db_folder = 'db'
-        store_name = pdf.name[:-4]
-        pkl_path = os.path.join(db_folder, f"{store_name}.pkl")
-        
-        if os.path.exists(pkl_path):
-            with open(pkl_path, "rb") as f:
-                VectorStore = pickle.load(f)
-            st.write('Embeddings loaded locally.')
-        else:
-            embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
 
+        if pdf is not None:
+            pdf_reader = PdfReader(pdf)
+            
+            text = ""
+            
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+                
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size = 1000,
+                chunk_overlap = 200,
+                length_function = len
+            )
+            
+            chunks = text_splitter.split_text(text = text)
+            
+            db_folder = 'db'
+            store_name = pdf.name[:-4]
             pkl_path = os.path.join(db_folder, f"{store_name}.pkl")
-
-            with open(pkl_path, "wb") as f:
-                pickle.dump(VectorStore, f)
-            st.write("New embeddings computed.")
             
+            if os.path.exists(pkl_path):
+                with open(pkl_path, "rb") as f:
+                    VectorStore = pickle.load(f)
+                st.write('Embeddings loaded locally.')
+            else:
+                embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+                VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+
+                pkl_path = os.path.join(db_folder, f"{store_name}.pkl")
+
+                with open(pkl_path, "wb") as f:
+                    pickle.dump(VectorStore, f)
+                st.write("New embeddings computed.")
+    
         select_llm = st.selectbox(
             'Select a large language model:',
             ('oasst-sft-4', 'flan-t5-xxl', 'falcon-7b')
@@ -75,21 +64,24 @@ def main():
             select_llm = 'google/flan-t5-xxl'
         elif select_llm == 'falcon-7b':
             select_llm = 'tiiuae/falcon-7b-instruct'
-            
-        st.write(select_llm)
         
-        query = st.text_input("Ask questions about your document:")
+    # st.write(select_llm)
+    
+    query = st.chat_input("Ask questions about your document...")
+    
+    if query:
+        with st.chat_message("user"):
+            st.write(query)
+        query = f"<|prompter|>{query}<|endoftext|><|assistant|>"          
+        docs = VectorStore.similarity_search(query = query, k = 4)           
+        llm = HuggingFaceHub(repo_id=select_llm, model_kwargs={"temperature":0.3, "max_length":200})
+        chain = load_qa_chain(llm = llm, chain_type = "stuff")
+        response = chain.run(input_documents = docs, question = query)
         
-        if query:
-            query = f"<|prompter|>{query}<|endoftext|><|assistant|>"          
-            docs = VectorStore.similarity_search(query = query, k = 4)           
-            llm = HuggingFaceHub(repo_id=select_llm, model_kwargs={"temperature":0.3, "max_length":200})
-            chain = load_qa_chain(llm = llm, chain_type = "stuff")
-            response = chain.run(input_documents = docs, question = query)
-            
+        with st.chat_message("ai"):
             st.write(response)
-            print(query)
-            print(response)
+        print(query)
+        print(response)
 
 if __name__ == '__main__':
     main()
