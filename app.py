865c9d394b9322901b6b42076b5861c5e66a634a
Meyrin <68102284+fahmi2000@users.noreply.github.com>
2023-11-21 13:35:20 +0800

    refactor get_conversation_chain

--- app.py
+++ app.py
@@ -8,19 +8,7 @@ from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.llms import HuggingFaceHub
 from langchain.chains.question_answering import load_qa_chain
-import concurrent.futures
 import os
-
-TIMING_ENABLE = False
-def timing_decorator(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        execution_time = end_time - start_time
-        print(f"{func.__name__}: {execution_time} seconds")
-        return result
-    return wrapper
 
 def get_pdf_text(pdf):
     pdf_reader = PdfReader(pdf)
@@ -42,20 +30,17 @@ def get_vectorstore(chunks, pdf_name):
     db_folder = 'db'
     pkl_path = os.path.join(db_folder, f"{pdf_name}.pkl")
 
-    with st.status("Processing document...") as status:
-        st.caption("Searching for existing embeddings...")
-        if os.path.exists(pkl_path):
-            with open(pkl_path, "rb") as f:
-                VectorStore = pickle.load(f)
-            status.update(label="Embeddings loaded!", state="complete", expanded=False)
-        else:
-            st.caption("Computing new data...")
-            embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-            VectorStore = FAISS.from_texts(chunks, embedding = embeddings)
+    if os.path.exists(pkl_path):
+        with open(pkl_path, "rb") as f:
+            VectorStore = pickle.load(f)
+        st.caption('Embeddings loaded locally.')
+    else:
+        embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+        VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
 
-            with open(pkl_path, "wb") as f:
-                pickle.dump(VectorStore, f)
-            status.update(label="New embeddings computed!", state="complete", expanded=False)
+        with open(pkl_path, "wb") as f:
+            pickle.dump(VectorStore, f)
+        st.caption("New embeddings computed.")
 
     return VectorStore
 
@@ -63,34 +48,34 @@ def get_conversation_chain(select_llm):
     model_mapping = {
         'oasst-sft-4': 'OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5',
         'flan-t5-xxl': 'google/flan-t5-xxl',
-        'flan-t5-small': 'google/flan-t5-small'
+        'falcon-7b': 'tiiuae/falcon-7b-instruct'
     }
 
     model_id = model_mapping.get(select_llm, select_llm)
-    llm = HuggingFaceHub(repo_id=model_id, model_kwargs={"temperature": 0.3, "max_token": 512})
+    llm = HuggingFaceHub(repo_id=model_id, model_kwargs={"temperature": 0.3, "max_length": 512})
     return load_qa_chain(llm=llm, chain_type="stuff")
 
-# @timing_decorator
-def process_response(query, VectorStore, select_llm):
-    chain = get_conversation_chain(select_llm)
-    query = f"<|prompter|>Please limit your responses to this document. Otherwise, you can say you do not know. According to this document, {query}<|endoftext|><|assistant|>"
-    with concurrent.futures.ThreadPoolExecutor() as exe:
-        future_vector_search = exe.submit(VectorStore.similarity_search, query=query, k=3)
-        docs = future_vector_search.result()
-        future_response = exe.submit(chain.run, input_documents=docs, question=query)
-    
-    return future_response.result()
 
-def display_response(response):
+def handle_user_input(query, VectorStore, select_llm):
+    query = f"<|prompter|>{query}<|endoftext|><|assistant|>" 
+    
+    docs = VectorStore.similarity_search(query=query, k=4)
+    
+    chain = get_conversation_chain(select_llm)
+    response = chain.run(input_documents=docs, question=query)
+    
+    print(query)
+    print(response)
+    
     with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            full_response = ""
-            assistant_response = response
-            for chunk in assistant_response.split():
-                full_response += chunk + " "
-                time.sleep(0.05)
-                message_placeholder.markdown(full_response + "▌")
-            message_placeholder.markdown(full_response)
+        message_placeholder = st.empty()
+        full_response = ""
+        assistant_response = response
+        for chunk in assistant_response.split():
+            full_response += chunk + " "
+            time.sleep(0.05)
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
 
 def main():
@@ -107,7 +92,7 @@ def main():
 
         select_llm = st.selectbox(
             'Select a large language model:',
-            ('oasst-sft-4', 'flan-t5-xxl', 'flan-ul2')
+            ('oasst-sft-4', 'flan-t5-xxl', 'falcon-7b')
         )
 
     if "messages" not in st.session_state:
@@ -122,11 +107,7 @@ def main():
             st.markdown(query)
         st.session_state.messages.append({"role": "user", "content": query})
         
-        response = process_response(query, VectorStore, select_llm)
-        display_response(response)
-        
-        st.cache_data.clear()
-        st.cache_resource.clear()
+        handle_user_input(query, VectorStore, select_llm)        
 
 if __name__ == '__main__':
     main()
