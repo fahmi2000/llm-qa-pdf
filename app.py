7b9c58217dba568a884c3eebf94b3bd2cad04a23
Meyrin <68102284+fahmi2000@users.noreply.github.com>
2023-11-24 15:50:34 +0800

    removed streamlit

--- app.py
+++ app.py
@@ -1,22 +1,14 @@
-import fitz as pymupdf
+from PyPDF2 import PdfFileReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.llms import HugggingFaceHub
+from langchain.llms import HuggingFaceHub
 from langchain.chains.question_answering import load_qa_chain
 import os
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
 import pickle
 
-# Load the pdf file and assigned the texts into a variable
-def get_pdf_text():
-    doc = pymupdf.open("klia.pdf")
-    text = ""
-    for page in doc.pages:
-        text += page.extract_text()
-    return text
-
 # Get the text variable and split it into sizes
-def get_text_chunks():
+def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size = 800,
         chunk_overlap = 100,
@@ -26,8 +18,8 @@ def get_text_chunks():
     chunks = text_splitter.split_text(text = text)
     return chunks
     
-def get_converse_chain():
-    llm = HugggingFaceHub(
+def load_llm_chain():
+    llm = HuggingFaceHub(
         repo_id = "HuggingFaceH4/zephyr-7b-beta",
         model_kwargs = {
             "temperature": 0.7,
@@ -42,11 +34,11 @@ def get_converse_chain():
     )
     
 # Look for existing embeddings before computing a new one
-def process_vectorstore(chunks, pdf_name):
+def process_vectorstore(chunks, pdf_path):
     db_folder = 'db'  
     pkl_path = os.path.join(
         db_folder,
-        f"{pdf_name}.pkl"
+        f"{pdf_path}.pkl"
     )
     
     if os.path.exists(pkl_path):
@@ -63,14 +55,15 @@ def process_vectorstore(chunks, pdf_name):
         
         with open(pkl_path, "wb") as f:
             pickle.dump(vectorstore, f)
+            
+    return vectorstore
     
+# Build the QA chain and pass it to the LLM
 def process_query(query, vectorstore):
-    chain = get_converse_chain()
+    chain = load_llm_chain()
     query = f"<|user|>\n{query}</s>"
     
-    docs = vectorstore.similarity_search(
-        query = query, k = 3
-    )
+    docs = vectorstore.similarity_search(query = query, k = 3)
     
     response = chain.run(
         input_documents = docs,
@@ -78,16 +71,32 @@ def process_query(query, vectorstore):
     )
     
     return response
-    
-# def display_reponse():
-
 
 def main():
-    get_pdf_text()
-    process_vectorstore()
-    query = input()
-    response = process_query
     
+    pdf = PdfFileReader("klia.pdf")
+    if pdf is not None:
+        try:
+            text = ""
+            for page in pdf.pages:
+                text += page.extract_text()
+                
+            chunks = get_text_chunks(text)
+            pdf_name = pdf.name[:-4]
+            vectorstore = process_vectorstore(chunks, pdf_name)
+            
+            while True:
+                
+                query = input("Enter your question:")
+                if query.lower() == 'exit':
+                    print("Exiting...")
+                    break
+                
+                response = process_query(query, vectorstore)
+                print(response)
+        
+        except Exception as e:
+            print(f"Error: {str(e)}")
     
 if __name__ == '__main__':
     main()
