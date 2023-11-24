ce4c8f3ffb05faa37a51936d0d86872cbefffd52
Meyrin <68102284+fahmi2000@users.noreply.github.com>
2023-11-24 17:25:27 +0800

    refactor to remove streamlit

--- app.py
+++ app.py
@@ -1,102 +1,91 @@
-from PyPDF2 import PdfFileReader
+from dotenv import load_dotenv
+from gtts import gTTS
+import pickle
+from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.llms import HuggingFaceHub
-from langchain.chains.question_answering import load_qa_chain
-import os
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
-import pickle
+from langchain.llms import HuggingFaceHub
+from langchain.chains.question_answering import load_qa_chain
+import concurrent.futures
+import os
 
-# Get the text variable and split it into sizes
+def get_pdf_text():
+    pdf_reader = PdfReader("klia.pdf")
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    return text
+
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 800,
-        chunk_overlap = 100,
-        length_function = len,
-        separators = "\n"
+        chunk_size=800,
+        chunk_overlap=200,
+        length_function=len,
+        separators="\n"
     )
     chunks = text_splitter.split_text(text = text)
     return chunks
-    
-def load_llm_chain():
+
+def get_vector_store(chunks):
+    db_folder = 'db'
+    pkl_path = os.path.join(db_folder, f"klia.pkl")
+
+    if os.path.exists(pkl_path):
+        with open(pkl_path, "rb") as f:
+            vector_store = pickle.load(f)
+
+    else:
+        embeddings = HuggingFaceInstructEmbeddings(model_name = "hkunlp/instructor-base")
+        vector_store = FAISS.from_texts(chunks, embedding = embeddings)
+
+        with open(pkl_path, "wb") as f:
+            pickle.dump(vector_store, f)
+
+    return vector_store
+
+def get_conversation_chain():
     llm = HuggingFaceHub(
-        repo_id = "HuggingFaceH4/zephyr-7b-beta",
-        model_kwargs = {
+        repo_id="HuggingFaceH4/zephyr-7b-beta",
+        model_kwargs={
             "temperature": 0.7,
-            "max_new_token": 1024,
+            "max_new_token": 512,
             "top_k": 50,
             "top_p": 0.95
         }
     )
-    return load_qa_chain(
-        llm = llm,
-        chain_type = "stuff"
-    )
+    return load_qa_chain(llm=llm, chain_type="stuff")
+
+def text_to_speech(text, language='en'):
+    tts = gTTS(text=text, lang=language, slow=False)
+    tts.save("response.mp3")
+    os.system("start response.mp3")
+
+def process_response(query, vector_store):
+    chain = get_conversation_chain()
+    query = f"<|system|>\nYou are a friendly customer service agent of Kuala Lumpur Internation Airport (KLIA).\n<|user|>\n{query}\n<|assistant|>"
+    with concurrent.futures.ThreadPoolExecutor() as exe:
+        future_vector_search = exe.submit(vector_store.similarity_search, query=query, k=3)
+        docs = future_vector_search.result()
+        future_response = exe.submit(chain.run, input_documents=docs, question=query)
     
-# Look for existing embeddings before computing a new one
-def process_vectorstore(chunks, pdf_path):
-    db_folder = 'db'  
-    pkl_path = os.path.join(
-        db_folder,
-        f"{pdf_path}.pkl"
-    )
-    
-    if os.path.exists(pkl_path):
-        with open(pkl_path, "rb") as f:
-            vectorstore = pickle.load(f)
-    else:
-        embeddings = HuggingFaceInstructEmbeddings(
-        model_name = "hkunlp/instructor-base"
-        )
-        
-        vectorstore = FAISS.from_texts(
-            chunks, embeddings = embeddings
-        )
-        
-        with open(pkl_path, "wb") as f:
-            pickle.dump(vectorstore, f)
-            
-    return vectorstore
-    
-# Build the QA chain and pass it to the LLM
-def process_query(query, vectorstore):
-    chain = load_llm_chain()
-    query = f"<|user|>\n{query}</s>"
-    
-    docs = vectorstore.similarity_search(query = query, k = 3)
-    
-    response = chain.run(
-        input_documents = docs,
-        question = query
-    )
-    
-    return response
+    return future_response.result()
 
 def main():
+    load_dotenv()
+    text = get_pdf_text()
+    chunk = get_text_chunks(text)
+    vector_store = get_vector_store(chunk)
     
-    pdf = PdfFileReader("klia.pdf")
-    if pdf is not None:
-        try:
-            text = ""
-            for page in pdf.pages:
-                text += page.extract_text()
-                
-            chunks = get_text_chunks(text)
-            pdf_name = pdf.name[:-4]
-            vectorstore = process_vectorstore(chunks, pdf_name)
-            
-            while True:
-                
-                query = input("Enter your question:")
-                if query.lower() == 'exit':
-                    print("Exiting...")
-                    break
-                
-                response = process_query(query, vectorstore)
-                print(response)
+    while True:
+        query = input("User input: ")
         
-        except Exception as e:
-            print(f"Error: {str(e)}")
-    
+        if query.lower() == 'exit':
+            break
+        
+        response = process_response(query, vector_store)
+        print(response)
+        text_to_speech(response)
+
 if __name__ == '__main__':
     main()
