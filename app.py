de753bf332798749c4d7b84bf7ba09dcc596f84c
Adelaide Meyrin <fahmialiff00@gmail.com>
2023-11-28 16:52:55 +0800

    Adjusted out-of-context logic
    - Removed cosine similarity
    - Change similarity_search to similarity_search_with_relevance_scores
    - Removed imports related to ingesting PDF files.

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from dotenv import load_dotenv
 import pickle
-from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
@@ -8,13 +7,6 @@ from langchain.llms import HuggingFaceHub
 from langchain.chains.question_answering import load_qa_chain
 import os
 import time
-
-def get_pdf_text():
-    pdf_reader = PdfReader("klia.pdf")
-    text = ""
-    for page in pdf_reader.pages:
-        text += page.extract_text()
-    return text
 
 def get_text_file():
     file_path = "klia.txt"
@@ -25,7 +17,7 @@ def get_text_file():
     except FileNotFoundError:
         print(f"Error: File '{file_path}' not found.")
         return None
-
+    
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size = 800,
@@ -57,7 +49,7 @@ def get_conversation_chain():
     llm = HuggingFaceHub(
         repo_id="HuggingFaceH4/zephyr-7b-beta",
         model_kwargs={
-            "temperature": 0.7,
+            "temperature": 0.3,
             "max_new_token": 512,
             "top_k": 50,
             "top_p": 0.95
@@ -68,11 +60,25 @@ def get_conversation_chain():
 def process_response(query, vector_store):
     chain = get_conversation_chain()
     query = f"<|system|>\nYou are a friendly customer service agent of Kuala Lumpur Internation Airport (KLIA).\n<|user|>\n{query}\n<|assistant|>"
-    docs = vector_store.similarity_search_with_score(query, k=3)
-    
-    response = chain.run(input_documents = docs, question = query)
+
+    docs = vector_store.similarity_search_with_relevance_scores(
+        query, 
+        k = 3,
+        score_threshold = 0.85
+    )
+
+    doc_submit = vector_store.similarity_search(query, k = 3)
+
+    print(docs)
+
+    if not docs:
+        response = "Sorry! I can only answer questions related to the Kuala Lumpur International Airport"
+
+    else:
+        response = chain.run(input_documents = doc_submit, question = query)
 
     return response
+    
 
 def main():
     load_dotenv()
@@ -88,7 +94,7 @@ def main():
         
         start_time = time.time()
         response = process_response(query, vector_store)
-        print(response)
+        print("--------------------\n", response, "\n--------------------")
         end_time = time.time()
         print("API Response Time: ", end_time - start_time)
 
